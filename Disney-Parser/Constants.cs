d92091c5188e10d52a8fea933355aacff4018c74
Pawel Gajzler <pawel.gajzler.pg@gmail.com>
2022-12-29 17:20:07 +0100

    added database manager and blank parsers

---
+++ Disney-Parser/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disney_Parser
+{
+    internal static class Constants
+    {
+        public static string ConnectionString = @"server=localhost;userid=dbuser;password=s$cret;database=testdb";
+    }
+}
---
+++ Disney-Parser/DatabaseManager.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Disney_Parser
+{
+    internal class DatabaseManager
+    {
+        public void ExecuteQuery(string query)
+        {
+            using MySqlConnection mySqlConnection = new(Constants.ConnectionString);
+            mySqlConnection.Open();
+
+            using MySqlCommand command = new();
+            command.Connection = mySqlConnection;
+            command.CommandText = query;
+
+            command.ExecuteNonQuery();
+        }
+
+        public int GetVoiceActorId(string query)
+        {
+            using MySqlConnection mySqlConnection = new(Constants.ConnectionString);
+            mySqlConnection.Open();
+
+            using MySqlCommand command = new();
+            command.Connection = mySqlConnection;
+            command.CommandText = query;
+
+            using MySqlDataReader reader = command.ExecuteReader();
+
+            while(reader.Read())
+            {
+                return reader.GetInt32(0);
+            }
+
+            return -1;
+        }
+    }
+}
