2cbab7d975753c3317861637bed59b53dd717a7d
proxna <pawel.gajzler.pg@gmail.com>
2022-12-30 23:26:53 +0100

    added voice actor parser

--- Disney-Parser/VoiceParser.cs
+++ Disney-Parser/VoiceParser.cs
@@ -8,6 +8,13 @@ namespace Disney_Parser
 {
     internal class VoiceParser
     {
+        private readonly DatabaseManager databaseManager;
+
+        public VoiceParser()
+        {
+            databaseManager = new();
+        }
+
         public void Parse(string path)
         {
             string fileContent = File.ReadAllText(path);
@@ -18,6 +25,8 @@ namespace Disney_Parser
                 string character = elements[1];
                 string voiceActor = elements[2];
                 string movingActor = elements[3];
+                string query = $"INSERT INTO VoiceActors VALUES('','{character}','{voiceActor}','{movingActor}')";
+                databaseManager.ExecuteQuery(query);
             }
         }
     }
